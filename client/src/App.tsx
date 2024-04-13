e0d13d7754e4de6ba3a3677b1a2949b38097e6b8
unknown <divyam.hs@somaiya.edu>
2024-04-13 12:06:02 +0530

    mui config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+
 function App() {
-
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    
     <div className='app'>
-
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+      </ThemeProvider>
     </div>
   )
 }
 
-export default App
+export default App;
