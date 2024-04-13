4cfae31d2806cd7b707662285403b408f798a2c4
Nidhi <nidhipoojary1211@gmail.com>
2024-04-13 15:11:59 +0530

    feat: dashboard layout

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Box, useMediaQuery } from "@mui/material";
+import DashboardBox from "../../components/DashboardBox";
 // import Row1 from "./Row1";
 // import Row2 from "./Row2";
 // import Row3 from "./Row3";
@@ -70,7 +71,16 @@ const Dashboard = () => {
             }
       }
     >
-      
+      <DashboardBox gridArea="a"></DashboardBox>
+      <DashboardBox gridArea="b"></DashboardBox>
+      <DashboardBox gridArea="c"></DashboardBox>
+      <DashboardBox gridArea="d"></DashboardBox>
+      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="f"></DashboardBox>
+      <DashboardBox gridArea="g"></DashboardBox>
+      <DashboardBox gridArea="h"></DashboardBox>
+      <DashboardBox gridArea="i"></DashboardBox>
+      <DashboardBox gridArea="j"></DashboardBox>
       {/* <Row1 />
       <Row2 />
       <Row3 /> */}
